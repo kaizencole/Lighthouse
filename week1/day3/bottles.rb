475de530abfbfc347635cd0bc97b426432418ded
vagrant <vagrant@lighthouse-labs>
2016-05-27 04:57:24 +0000

    Bottles improved

--- week1/day3/bottles.rb
+++ week1/day3/bottles.rb
@@ -1,32 +1,37 @@
 #TASK 1
 
 def buy_bottles(dollars)
-  (dollars / 2).divmod(1) = bottles
-  bottles = o_bottles
-  @recycled = []
+  bottles = (dollars/2).divmod(1)
+  o_bottles = bottles 
+  recycled = []
 
-  while loop = true
-    loop = true
+loop = true
 
-    (bottles / 2).divmod(1) + (bottles / 4).divmod(1) = r_bottles
+  while loop
 
-    r_bottles + (bottles % 2) = empty
-    r_bottles + (bottles % 4) = caps 
+    r_bottles = (bottles/2).divmod(1) + (bottles/4).divmod(1)
 
-    @recycled.append(r_bottles)
+    empty = r_bottles + (bottles % 2)
+    caps = r_bottles + (bottles % 4) 
+
+    recycled.push(r_bottles)
 
     if (empty / 2) >= 1
       loop = true
+      bottles = r_bottles
 
       else
         loop = false
     end
   end
 
-  @recycled.sum = r_bottles
-  o_bottles + r_bottles = bottles
+  recycled.inject(0){|sum, r_bottles| sum + r_bottles }
+  o_bottles + r_bottles = total_bottles
 
   puts "With #{dollars}, you can buy #{o_bottles} bottles of beer, 
-  plus #{r_bottles} recycled bottles, to make #{bottles} total bottles."
+  plus #{r_bottles} recycled bottles, to make #{total_bottles} total bottles."
 end  
 
+#test
+dollars = 20
+puts buy_bottles(dollars)
